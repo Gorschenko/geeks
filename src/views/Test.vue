fc4a1ce92d5e039ea4c9608831ac813437dd6a9d
Gorschenko <aa.gorschenko@yandex.ru>
2022-02-22 22:35:46 +0300

    Добавлена пагинация в компненте Test

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,21 +1,36 @@
 <template>
 <section class="test">
+  <div class="inner">
 
-  <div class="test__header">
-    <button
-      class="button button_success"
-      @click="loadPosts"
-    >Загрузить список</button>
-  </div>
+    <div class="test__header">
+      <button
+        class="button button_success mr-1"
+        @click="loadPosts"
+      >Загрузить список</button>
+      <ol class="pages">
+        <li
+        v-for="pageNumber in totalPages"
+        :key="pageNumber"
+        :class="{
+          'pages__item': true,
+          'text_smallest': true,
+          'pages__item_active': page === pageNumber
+        }"
+        @click="page = pageNumber"
+        >{{ pageNumber }}</li>
+      </ol>
+    </div>
 
-  <div class="test__body">
-    <ul class="list">
-      <li class="list__item" v-for="post in posts">
-        <span class="text text_small text_bold text_primary mb-05">{{ post.id }}</span>
-        <h1 class="text text_smallest mb-05">{{ post.title }}</h1>
-        <p class="text text_smallest">{{ post.body }}</p>
-      </li>
-    </ul>
+    <div class="test__body">
+      <ul class="list">
+        <li class="list__item" v-for="post in posts">
+          <span class="text text_small text_bold text_primary mb-05">{{ post.id }}</span>
+          <h1 class="text text_smallest mb-05">{{ post.title }}</h1>
+          <p class="text text_smallest">{{ post.body }}</p>
+        </li>
+      </ul>
+    </div>
+
   </div>
 </section>
 </template>
@@ -25,15 +40,32 @@ import axios from 'axios'
 export default {
 data() {
   return {
-    posts: []
+    posts: [],
+    page: 1,
+    limitPosts: 10,
+    totalPages: 0
   }
+},
+mounted() {
+  this.loadPosts()
 },
 methods: {
   async loadPosts() {
     try {
-      const {data} = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=50')
-      this.posts = data
+      const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+        params: {
+          _page: this.page,
+          _limit: this.limitPosts
+        }
+      })
+      this.posts = response.data
+      this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limitPosts)
     } catch(e) {}
+  }
+},
+watch: {
+  page() {
+    this.loadPosts()
   }
 }
 }
@@ -42,9 +74,23 @@ methods: {
 <style lang="scss" scoped>
 .test {
   &__header {
+    display: flex;
     margin-bottom: 1rem;
   }
 }
+
+.pages {
+  display: flex;
+  &__item {
+    padding: 10px;
+    outline: $border-secondary;
+    cursor: pointer;
+    &_active {
+      background: $color-success;
+    }
+  }
+}
+
 .list {
   &__item {
     @include container;
@@ -52,4 +98,6 @@ methods: {
     margin: 0 auto 1rem;
   }
 }
+
+
 </style>
